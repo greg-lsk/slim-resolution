a0468c8c17b7bdf4be9eb0673d4ff451b0e5425e
Greg Liaskas <greg.liaskas@outlook.com>
2026-04-10 17:00:18 +0300

    update:: Scaffold of how to auto-register defined metadata types to the DI-container.

---
+++ SlimResolution.Core/.src/MetadataRegistration/RegistrationContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SlimResolution.Core.MetadataRegistration;
+
+public class RegistrationContext
+{
+    public IEnumerable<MetadataInfo> MetadataInfo { get; }
+
+
+    private RegistrationContext()
+    {
+        var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        var dllFiles = Directory.GetFiles(baseDir, "*.dll");
+
+        List<MetadataInfo> infos = [];
+
+        for (int i = 0; i < dllFiles.Length; i++)
+        {
+            var assembly = Assembly.LoadFrom(dllFiles[i]);
+            var assemblyTypes = assembly.GetTypes();
+
+            UpdateMetadataInfo(infos, assemblyTypes);
+        }
+
+        MetadataInfo = infos;
+    }
+    public static RegistrationContext Instance => new();
+
+
+    private void UpdateMetadataInfo(List<MetadataInfo> infos, IEnumerable<Type> types)
+    {
+        foreach(var type in types)
+        {
+            if (type.IsValueType || type.IsAbstract || type.IsInterface) continue;
+
+            var targetType = type.GetInterfaces()
+                                 .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IResolutionMetadata<>))
+                                 .Select(i => i.GetGenericArguments()[0])
+                                 .FirstOrDefault();
+
+            if (targetType is null) continue;
+            infos.Add(new MetadataInfo(targetType, type));
+        }
+    }
+}
